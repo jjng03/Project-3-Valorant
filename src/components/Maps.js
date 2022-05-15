2db85bd309f415248907492f8900367020c20a12
James Jung <jjng930@gmail.com>
2022-05-14 22:41:58 -0500

    created Maps component

---
+++ src/components/Maps.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Maps() {
+    return (
+        <div className="section1">
+            <div className="maps">
+                <h1>Maps</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Maps;
