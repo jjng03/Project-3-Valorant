6e4ec4e9f4ad12f85b27e2a3ba03bf57c8bd5419
James Jung <jjng930@gmail.com>
2022-05-14 23:04:50 -0500

    created a getAgents function to fetch API and return json data

--- src/components/Agents.js
+++ src/components/Agents.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Agents() {
+    const [agents, setAgents] = useState("null");
+
+    const url = 'https://valorant-api.com/v1/agents';
+    async function getAgents() {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        setAgents(data)
+    }
+    useEffect(()=>{
+        getAgents();
+    }, []);
+
     return (
         <div className="section1">
             <div className="agents">
