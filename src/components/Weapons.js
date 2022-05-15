58a5fa25c517936689271cc1e3ee488a2d158464
James Jung <jjng930@gmail.com>
2022-05-14 22:45:22 -0500

    created Weapons component

---
+++ src/components/Weapons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Weapons() {
+    return (
+        <div className="section1">
+            <div className="weapons">
+                <h1>Weapons</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Weapons;
