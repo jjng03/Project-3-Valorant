57180aad6788533222e1846666be22969f6eb3b6
James Jung <jjng930@gmail.com>
2022-05-15 00:15:48 -0500

    mapped and displaying all agent images on the browser

--- src/components/Agents.js
+++ src/components/Agents.js
@@ -1,24 +1,33 @@
 import React, {useState, useEffect} from 'react';
+import '../styles/Agents.css';
 
 function Agents() {
-    const [agents, setAgents] = useState("null");
+    const [agents, setAgents] = useState([]);
 
     const url = 'https://valorant-api.com/v1/agents';
-    async function getAgents() {
+    const getAgents = async () => {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data)
-        setAgents(data)
+        // console.log(data)
+        setAgents(data.data)
     }
     useEffect(()=>{
         getAgents();
     }, []);
 
+    console.log(agents)
     return (
-        <div className="section1">
-            <div className="agents">
-                <h1>Agents</h1>
-            </div>
+        <div className="agent-container">
+            {
+                agents.map((agent) => (
+                    // console.log(agent)
+                    <div className="agents">
+                        <div className="agent-image">
+                            <img src={agent.fullPortraitV2} alt={agent.displayName} />
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
