18b1a835c9c033bda2a505ecf07807f0f2068983
James Jung <jjng930@gmail.com>
2022-05-16 12:37:18 -0500

    created a handleCurrent function to grab data from a specific agent and changed layout of the agents page

--- src/components/Agents.js
+++ src/components/Agents.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import '../styles/Agents.css';
+import { Link } from 'react-router-dom';
 
 function Agents() {
     const [agents, setAgents] = useState([]);
+    // const [results, setResults] = useState([]);
+    const [current, setCurrent] = useState({});
 
     const url = 'https://valorant-api.com/v1/agents';
     const getAgents = async () => {
@@ -11,24 +14,54 @@ function Agents() {
         // console.log(data)
         setAgents(data.data)
     }
+
     useEffect(()=>{
         getAgents();
     }, []);
 
-    console.log(agents)
+    const handleCurrent = (e)=>{
+        console.log(e.target.innerText)
+        const findCurrent = agents.find(
+            (agent)=>agent.displayName.toUpperCase() === e.target.innerText
+            );
+            console.log(findCurrent)
+            setCurrent(findCurrent)
+    }
+    // console.log(agents)
     return (
-        <div className="agent-container">
-            {
-                agents.map((agent) => (
-                    // console.log(agent)
-                    <div className="agents">
-                        <div className="agent-image">
-                            <img src={agent.fullPortraitV2} alt={agent.displayName} />
+        <div className="agent-section">
+            <div className="column1">
+                {
+                    agents && agents.map((agent) => (
+                        <Link to={`/agents`} key={ agent.uuid } onClick={handleCurrent}>
+                            <div className="agent-name">
+                                {(agent.isPlayableCharacter === true) ? <h1>{agent.displayName}</h1> : null}
+                            </div>
+                        </Link>
+                    ))
+                }
+            </div>
+            <div className="column2">
+                {
+                    agents.map((agent) => ( 
+                        <div className="agent-name">
+                            {(agent.displayName === "Fade") ? <img src={agent.fullPortraitV2}/> : null}
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
+            <div className="column3">
+                {
+                    agents.map((agent) => (
+                        <div className="agent-name">
+                            {(agent.displayName === "Fade") ? <p>{agent.description}</p> : null}
+                        </div>
+                    ))
+                }
+            </div>
         </div>
+            
+        
     )
 }
 
