50bec8f275a868458c353be69713e00fd79546ce
James Jung <jjng930@gmail.com>
2022-05-17 14:14:41 -0500

    showing each name for maps

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import '../styles/Maps.css'
+import { Link } from 'react-router-dom';
 
 function Maps() {
     const [maps, setMaps] = useState([]);
+    const [currentMap, setCurrentMap] = useState([]);
 
     const url = 'https://valorant-api.com/v1/maps';
     const getMaps = async () => {
@@ -11,30 +13,51 @@ function Maps() {
         // console.log(data)
         setMaps(data.data)
     }
-    console.log(maps)
+    // console.log(maps)
 
     useEffect(()=>{
         getMaps();
     }, []);
 
+    const handleCurrentMap = (e)=>{
+        console.log(e.target.alt)
+        const findCurrentMap = maps.find(
+            // targeting the alt of the img tag
+            (map)=>map.displayName === e.target.alt
+            );
+            console.log(findCurrentMap)
+            setCurrentMap(findCurrentMap)
+    }
+
+    const handleScroll = (e)=>{
+        console.log(e.target.scrollWidth, e.target.scrollLeft)
+
+    }
+
     return (
         <div className="map-main">
             <h6>MAPS</h6>
-            <div className="map-section snaps-inline">
-                {
-                    maps && maps.map((map)=> (
-                        <div className="allMaps">
-                            <img src={map.splash} className="every-map" alt=""/>
-                            {/* <h6>{map.displayName}</h6> */}
-                        </div>
-                    ))
-                }
-                {/* <div className="allMaps">
-                    <h6>Maps</h6>
-                </div> */}
-            </div>
-            <div className="map-info">
-                <h7>Hello</h7>
+            <div className="button-section">
+                <button className="back">BACK</button>
+                <button className="next">NEXT</button>
+                <div className="map-section snaps-inline" onScroll={handleScroll}>
+                    {
+                        maps && maps.map((map)=> (
+                            <div className="all-maps">
+                                {/* <Link to='/maps' key={ map.uuid } onClick={handleCurrentMap}> */}
+                                    <img src={map.splash} className="every-map" alt={map.displayName} onClick={handleCurrentMap}/>
+                                {/* </Link> */}
+                            </div>
+                        ))
+                    }
+            
+                </div>
+                <div className="map-info">
+                    <p className="map-name">{ currentMap.displayName }</p>
+                    <Link to='/maps/layout' key={ currentMap.uuid } onClick={handleCurrentMap}>
+                        <p className="layout">view layout</p>
+                    </Link>
+                </div>
             </div>
         </div>
     )
